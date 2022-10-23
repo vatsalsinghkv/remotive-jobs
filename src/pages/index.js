37d1c97ea0ea10ee3577f8dc446eb2a897a48c5d
Vatsal Singh <vatsalsinghkv@gmail.com>
2022-10-23 10:04:34 +0530

    fix: ssr

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,28 +9,32 @@ import {
   setSelectedCategory,
 } from '../store/jobs';
 import { getSeoData } from '../lib/utils/portfolio';
-import { REVALIDATE_TIME } from '../lib/utils/constant';
 
-export const getStaticProps = wrapper.getStaticProps((store) => async () => {
-  const jobs = await getAllJobs();
-  store.dispatch(setJobs(jobs));
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    const jobs = await getAllJobs();
+    store.dispatch(setJobs(jobs));
 
-  const categories = await getJobsCategories();
-  store.dispatch(setCategories(categories));
+    const categories = await getJobsCategories();
+    store.dispatch(setCategories(categories));
 
-  const locations = await getLocations();
-  store.dispatch(setLocations(locations));
+    const locations = await getLocations();
+    store.dispatch(setLocations(locations));
 
-  store.dispatch(setSelectedCategory('all'));
+    store.dispatch(setSelectedCategory('all'));
 
-  return {
-    revalidate: 10,
-    // notFound: true, // When you gave to return 404 page
-    // redirect: {
-    //   destination: '/another-route',
-    // },
-  };
-});
+    return {
+      props: {
+        jobs,
+      },
+      // revalidate: 10,
+      // notFound: true, // When you gave to return 404 page
+      // redirect: {
+      //   destination: '/another-route',
+      // },
+    };
+  }
+);
 
 export default function Home() {
   return (
